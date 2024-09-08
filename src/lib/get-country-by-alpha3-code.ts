9cff0e87e73885c3bb69961151b67f797e9b448e
Siraj Sharma <siraj.sharma@paxcom.net>
2024-09-09 00:14:52 +0530

    Moved data.json to public folder

--- src/lib/get-country-by-alpha3-code.ts
+++ src/lib/get-country-by-alpha3-code.ts
@@ -3,7 +3,7 @@ import { promises as fs } from "fs";
 export async function getCountryByAlpha3Code(
   alpha3Code: string
 ): Promise<Country | string> {
-  const file = await fs.readFile(`${process.cwd()}/src/app/data.json`);
+  const file = await fs.readFile(`${process.cwd()}/data.json`);
   const data = JSON.parse(file as unknown as string) as Country[];
 
   const country = data.find((country) => country.alpha3Code === alpha3Code);
